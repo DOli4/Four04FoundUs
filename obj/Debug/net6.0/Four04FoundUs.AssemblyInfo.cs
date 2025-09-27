59cc89f5e40d64c8e7f57de0d0ba7ed1e6b1a7ec
DieMalEen <142520405+DieMalEen@users.noreply.github.com>
2025-09-27 15:34:45 +0200

    New bot

--- obj/Debug/net6.0/Four04FoundUs.AssemblyInfo.cs
+++ obj/Debug/net6.0/Four04FoundUs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Four04FoundUs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746fdb0f03e19a0dbea80f304ffc56af6981d7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3379bb9676a2ecb922ec4925ae0815dbca14d7aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Four04FoundUs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Four04FoundUs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
